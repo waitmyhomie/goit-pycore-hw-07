11ca997654f8312c0ac9d309bd5140b7a01a3c5f
waitmyhomie <jabs@faber-solartechnik.de>
2024-10-28 17:40:25 +0100

    fixes and task 1 done

--- homework.py
+++ homework.py
@@ -1,4 +1,5 @@
 from collections import UserDict
+from datetime import datetime, timedelta
 
 class Field:
     def __init__(self, value):
@@ -24,16 +25,23 @@ class Phone(Field):
     
 class Birthday(Field):
     def __init__(self, value):
-        if self.validate_birthday(value):
-            super().__init__(value)
-        else:
-            raise ValueError("Birthday must be in format YYYY-MM-DD.")
+        try:
+            self.value=datetime.strptime(value, "%d.%m.%Y")
+        except ValueError:
+            raise ValueError("Birthday must be in format DD.MM.YYYY")  
+            
+        
     @staticmethod
     def validate_birthday(value):
-        print(len(value) == 10 and value[4] == "-" and value[7] == "-" and value[:4].isdigit() and value[5:7].isdigit() and value[8:].isdigit())
-        return len(value) == 10 and value[4] == "-" and value[7] == "-" and value[:4].isdigit() and value[5:7].isdigit() and value[8:].isdigit()
-
-
+        try:
+            day, month, year = value.split(".")
+            if len(year) == 4 and len(day) == 2 and len(month) == 2:
+                return True
+            else:
+                return False
+        except ValueError:
+            return False
+        
 class Record:
     def __init__(self, name):
         self.name = Name(name)
@@ -73,15 +81,23 @@ class Record:
             if p.value == phone:
                 return p
         return None
-
-    def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
     
     def add_birthday(self, birthday):
         try:
-            self.birthday = Birthday.validate_birthday(birthday)
+            self.birthday = Birthday(birthday)
         except ValueError as e:
             print(e)
+            
+    def change_birthday(self, birthday):
+        try:
+            self.birthday = Birthday(birthday)
+        except ValueError as e:
+            print(e)
+            
+    def __str__(self):
+        phones_str = '; '.join(p.value for p in self.phones)
+        birthday_str = f", birthday: {self.birthday.value.strftime('%d.%m.%Y')}" if self.birthday else ""
+        return f"Contact name: {self.name.value}, phones: {phones_str}{birthday_str}"
 
 class AddressBook(UserDict):
     def add_record(self, record):
@@ -96,44 +112,64 @@ class AddressBook(UserDict):
             return True
         print("Record not found.")
         return False
-
+    
+    def get_upcoming_birthdays(self, days=7):
+        upcoming_birthdays = []
+        today = datetime.now()
+        next_week = today + timedelta(days=days)
+        for record in self.data.values():
+            if record.birthday:
+                birthday_this_year = record.birthday.value.replace(year=today.year)
+                if today <= birthday_this_year <= next_week:
+                    upcoming_birthdays.append(record)
+        return upcoming_birthdays
 
 if __name__ == "__main__":
     book = AddressBook()
     # user john
-    user_record = Record("John")
+    user_record = book.find("John")
+    if not user_record:
+        user_record = Record("John")
     user_record.add_phone("1234567890")
     user_record.add_phone("5555555555")
-    #pass a function to add birthday
-    user_record.add_birthday("01-01-1990")
+    user_record.add_birthday("01.02.1990")
 
   
     book.add_record(user_record)
+    
     #user jane
-    user_record = Record("Jane")
-    user_record.add_phone("9876543210")
-    book.add_record(user_record)
+    user_record = book.find("Jane")
+    if not user_record:
+        user_record = Record("Jane")
+    user_record.add_phone("1234567892")
+    user_record.add_phone("1234567891")
+    user_record.add_birthday("01.02.1993")
 
+    book.add_record(user_record)
+    
+    jane = book.find("Jane")
+    if jane:
+        jane.change_birthday("01.02.1994")
  
     for name, record in book.data.items():
         print(record)
 
-    #search john and edit phone
-    john = book.find("John")
-    if john:
-        john.edit_phone("1234567890", "1112223333")
+   #search john and edit phone
+    # john = book.find("John")
+    # if john:
+    #     john.edit_phone("1234567890", "1112223333")
     
-    print(john) 
+    # print(john) 
 
-    found_phone = john.find_phone("5555555555")
-    if found_phone:
-        print(f"{john.name}: {found_phone}")  
+    # found_phone = john.find_phone("5555555555")
+    # if found_phone:
+    #     print(f"{john.name}: {found_phone}")  
 
-    # delete jane
-    book.delete("Jane")
+    # # delete jane
+    # book.delete("Jane")
 
-    jane = book.find("Jane")
-    if not jane:
-        print("Jane not found.")
-    else:
-        print(jane)
+    # jane = book.find("Jane")
+    # if not jane:
+    #     print("Jane not found.")
+    # else:
+    #     print(jane)
